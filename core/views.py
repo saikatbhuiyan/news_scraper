0d31e5acff1c919ebe88292245c0d306fb286811
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-10-01 13:35:20 +0600

    create news model and list view

--- core/views.py
+++ core/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-from core.tasks import send_mass_emails
+# from core.tasks import send_mass_emails
 
-# Create your views here.
+# # Create your views here.
 
 
-def mass_email_view(request):
-    recipient = 'fuewhfuew@fewfe.com'
-    print("Received request")
-    send_mass_emails.delay(recipient)
-    return render(request, "index.html")
+# def mass_email_view(request):
+#     recipient = 'fuewhfuew@fewfe.com'
+#     print("Received request")
+#     send_mass_emails.delay(recipient)
+#     return render(request, "index.html")
+
+
+from django.views import generic
+from .models import NewsItem
+
+
+class NewsItemListView(generic.ListView):
+    model = NewsItem
+    template_name = 'news_item_list.html'
