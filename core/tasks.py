62317847b892a3c0e6e4b7cbaa7b72f8c323f1f0
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-10-02 00:59:25 +0600

    scrape async task

--- core/tasks.py
+++ core/tasks.py
@@ -3,14 +3,21 @@ from newsscraper.celery  import app as celery_app
 from .scrapers import scrape
 
 
+URL = "https://dev.to/search?q=django"
+
+
 @celery_app.task
 def scrape_dev_to():
-    URL = "https://dev.to/search?q=django"
     scrape(URL)
     return
 
 
-# allows you to call this function asynchronously
+@shared_task  # allows us to call this function asynchronously
+def scrape_async():
+    scrape(URL)
+    return
+
+# allows us to call this function asynchronously
 
 
 # @shared_task
